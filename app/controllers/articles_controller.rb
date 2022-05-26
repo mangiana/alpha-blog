5bbef927f2be3160603616e4604b3215bea88541
iftikhar <iftikharmangiana.official@gmail.com>
2022-05-26 14:27:02 +0500

    Create new and create methods/actions and their respective routes, and built a form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,5 +7,18 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def new
+    
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article.save
+    redirect_to article_path(@article) #yahn pur (@article) by defualt id dy ga, or hm ny isy waja sy use kia ha ku k hamein show path main id chahiye
+    # rediret_to article_path(@article) can also be written as
+    # redirect_to @article it will perfom the same function
+
+  end
+
 
 end
